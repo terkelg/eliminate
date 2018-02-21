9a505cd27906437582ca189a3d96537f63ca0dca
Terkel Gjervig <terkel@terkel.com>
2018-02-20 21:24:49 -0800

    formatting

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -2,16 +2,16 @@ const path = require('path');
 const fs = require('fs');
 
 function mkdirp(dir) {
-	const parent = path.dirname(dir);
-	if (parent === dir) return;
+  const parent = path.dirname(dir);
+  if (parent === dir) return;
 
-	mkdirp(parent);
+  mkdirp(parent);
 
-	try {
-		fs.mkdirSync(dir);
-	} catch (err) {
-		if (err.code !== 'EEXIST') throw err;
-	}
+  try {
+    fs.mkdirSync(dir);
+  } catch (err) {
+    if (err.code !== 'EEXIST') throw err;
+  }
 }
 
 module.exports = mkdirp;
