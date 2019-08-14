fb5a564a0ba71eefc128333ce709c5d83ae6e749
Terkel Gjervig <terkel@terkel.com>
2019-08-14 11:52:55 +0200

    Added CLI

---
+++ src/bin.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+const del = require('./');
+
+const help = `
+Examples
+ $ eliminate [path]
+ $ eliminate path/to/file.md
+ $ eliminate path/to/directory
+`;
+
+let args = process.argv.slice(2);
+
+if (args.includes('--help') || args.includes('-h')) {
+  console.log(help);
+  process.exit();
+}
+
+if (args[0]) {
+	del(args[0])
+		.then(() => process.exit())
+		.catch(() => {
+			console.log(`Could not eliminate '${args[0]}'`);
+			process.exit(1)
+		})
+} else {
+	console.log('No path provided');
+	process.exit();
+}
